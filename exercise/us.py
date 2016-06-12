8c48eef24c65b0b919e83831f957ea3624f14747
feipai11 <2013301020066@whu.edu.cn>
2016-06-12 11:19:37 +0800

    add first homework

---
+++ exercise/us.py
@@ -0,0 +1,7 @@
+import os                                                                        
+import math                                                                      
+import time
+
+f = open('/homework/computationalphysics_N2013301020066/us.txt', 'r')
+st=  f.read()
+print st
